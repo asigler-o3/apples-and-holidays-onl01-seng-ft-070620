1594cc8c11acc1e9071298e86bd691a1b3bc5dc6
Ashley Sigler <ashleysigler13@gmail.com>
2020-07-18 08:24:58 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -75,6 +75,8 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |event, supplies|
       if supplies.includes?("BBQ")
+        event
+      end
     end
   end
 end
